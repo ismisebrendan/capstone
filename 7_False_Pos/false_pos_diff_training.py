0989a1ecdd84ae58042bba914e755e25f4968d53
Brendan <wattersb@tcd.ie>
2024-10-01 10:35:06 +0100

    Updated spectrum_obj.py
    Added the ability to generate spectra with random lines removed and created files to generate this data for training/learning

---
+++ 7_False_Pos/false_pos_diff_training.py
@@ -0,0 +1,41 @@
+import os
+#numpy + pyplot together
+import numpy as np
+#tensorflow with keras wrapper
+import tensorflow as tf
+from tensorflow.keras.layers import Dense
+from tensorflow.keras.models import Sequential
+
+
+
+# Import data
+# file = "false_pos-20240930T161616.txt"
+
+training_data = np.loadtxt(file)
+
+# Using same settings as Andrew
+# Create the Network using the Keras 
+model = Sequential()
+model.add(Dense(8, activation = "tanh", kernel_initializer='random_normal'))
+#Adding hidden layers
+model.add(Dense(379, activation = "tanh", kernel_initializer='random_normal'))
+model.add(Dense(253, activation = "tanh", kernel_initializer='random_normal'))
+#Adding output layer
+model.add(Dense(1, activation = "sigmoid", kernel_initializer='random_normal'))
+model.compile(loss = "mean_squared_error", optimizer='adam', metrics=['accuracy'])
+
+# Train the ANN using stochastic gradient descent
+# with a validation split of 20%
+model.fit(training_data[:,:-1], training_data[:,-1], epochs=500, batch_size=64, verbose=1, validation_split=0.2)
+
+
+# Evaluate the model accuracy
+_, accuracy = model.evaluate(training_data[:,:-1], training_data[:,-1])
+
+
+
+#save the trained network
+filepath = os.path.splitext(file)[0]
+name = filepath.split("-")[0]
+print("%0.3f" % accuracy)
+model.save(f'{name}.keras')
--- 7_False_Pos/gen_data_diff_amp.py
+++ 7_False_Pos/gen_data_diff_amp.py
@@ -9,16 +9,11 @@ import argparse
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Generate and fit synthetic spectra using the Spectrum object for both actual amplitudes and false positives for use when training an ANN.')
-    parser.add_argument('-n', '--Nsim', type=int, help='The number of real spectra to generate.', default=10)
-    parser.add_argument('-f', '--false', type=int, help='The number of false positives to generate (default is the same as Nsim).', default=None)
+    parser.add_argument('-n', '--Nsim', type=int, help='The number of real spectra to generate.', default=10000)
     parser.add_argument('-o', '--file_out', type=str, help='The filename to save this data to.', default=None)
     args = parser.parse_args()
     
     Nsim = args.Nsim
-    if args.false == None:
-        false = Nsim
-    else:
-        false = args.false
 
     # Generate Spectrum object
     spec = Spectrum('lines_in.txt', 'fitting.txt', Nsim=Nsim, AoN_min=3)
@@ -29,29 +24,17 @@ if __name__ == "__main__":
 
     # Generate actual amplitudes
     print('Generating spectral data')
-    spec.simulation()
+    spec.simulation_false()
     AoN_out = spec.AoNs_out
+    lines_present = spec.keep_lines
 
     # Save data
-    data_out = np.transpose(np.concatenate((AoN_out, np.ones((1, Nsim)))))
+    data_out = np.transpose(np.concatenate((AoN_out, lines_present)))
 
-    # Generate false positives
-    print('Generating false positives')
-    spec.AoN_min = 0
-    spec.AoN_max = 0
-    spec.Nsim = false
-    spec.simulation()
-    AoN_out = spec.AoNs_out
-
-    # Save data
-    empty_data = np.transpose(np.concatenate((AoN_out, np.zeros((1, false)))))
-    data_out = np.concatenate((data_out, empty_data))
-    
-    np.random.shuffle(data_out)
 
     if args.file_out == None:
         date = datetime.now()
-        file_out = f'false_pos-{date.strftime("%Y%m%dT%H%M%S")}.txt'
+        file_out = f'false_pos_diff-{date.strftime("%Y%m%dT%H%M%S")}.txt'
     else:
         file_out = args.file_out
     
