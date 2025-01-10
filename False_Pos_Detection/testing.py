f1e105c95cd27d567a6f10c714d44871cd35581e
Brendan <135332281+ismisebrendan@users.noreply.github.com>
2025-01-10 15:12:37 +0000

    Update testing.py

--- False_Pos_Detection/testing.py
+++ False_Pos_Detection/testing.py
@@ -3,7 +3,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import tensorflow as tf
 
-# The cutoff for what is considered a line
+# The cutoff for what is considered a spectrum
 cutoff = 0.8
 
 outputs = 1
@@ -39,22 +39,15 @@ predicted_data = np.hstack((test_data, YPred))
 potential = predicted_data[np.unique(np.where(YPred > cutoff)[0]),:]
 # np.savetxt(f'{ANN_path}/Potential_Detections_in_{test_name}_with_{ANN_name}.dat', potential)
 
-# cutoffs = np.arange(0.5, 1.1, 0.1)
-
-# for cutoff in cutoffs:
 # Create histograms for each peak
-for i in range(len(YPred[0])):
-    hbin = np.linspace(min(YPred[:,i]), max(YPred[:,i]), num=100).reshape(-1)
-    plt.hist(YPred[np.where(YPred[:,i] < cutoff)[0]][:,i], bins=hbin, density=False, label='Non-Detections')
-    plt.hist(YPred[np.where(YPred[:,i] > cutoff)[0]][:,i], bins=hbin, density=False, label='Potential Detections')
-    plt.xlabel('Predicted likelihood of being a real detection by network')
-    plt.ylabel('Number of spectra')
-    plt.title(f'{ANN_name} trained network predicting {test_name} for peak {i}')
-    plt.yscale('log')
-    plt.show()
-    # print(f'Found peak {i} in {len(np.where(YPred[:,i] > cutoff)[0])/len(YPred) *100}% of spectra')
-    # print(f'Expected value: {np.unique(lines_present[i], return_counts=True)[1][1] / len(YPred) * 100}')
-    # print(f'Accuracy: {len(np.where(YPred[:,i] > cutoff)[0]) / np.unique(lines_present[i], return_counts=True)[1][1]}\n' )
+hbin = np.linspace(min(YPred), max(YPred), num=100).reshape(-1)
+plt.hist(YPred[np.where(YPred < cutoff)[0]], bins=hbin, density=False, label='Non-Detections')
+plt.hist(YPred[np.where(YPred > cutoff)[0]], bins=hbin, density=False, label='Potential Detections')
+plt.xlabel('Predicted likelihood of being a real detection by network')
+plt.ylabel('Number of spectra')
+plt.title(f'{ANN_name} trained network predicting {test_name} for peak {i}')
+plt.yscale('log')
+plt.show()
 
 # Separate the lines into the different categories
 lines_present = lines_present.T
@@ -77,11 +70,11 @@ ax.xaxis.set_ticks_position('none')
 ax.yaxis.set_ticks_position('none')
 plt.imshow(confusion, cmap='Greys')
 plt.colorbar()
-plt.xticks([0, 1], ['Line present', 'Line not present'])
-plt.yticks([0, 1], ['Line detected', 'Line not detected'], rotation='vertical', va='center')
-plt.ylabel('Is the line detected?')
-plt.xlabel('Is the line present?')
-plt.title(f'Confusion matrix for all lines in {test_name}\nusing {ANN_name}')
+plt.xticks([0, 1], ['Spectrum present', 'Spectrum not present'])
+plt.yticks([0, 1], ['Spectrum detected', 'Spectrum not detected'], rotation='vertical', va='center')
+plt.ylabel('Is the spectrum detected?')
+plt.xlabel('Is the spectrum present?')
+plt.title(f'Confusion matrix for all spectra in {test_name}\nusing {ANN_name}')
 plt.text(0, 0, int(confusion[0][0]), ha='center', backgroundcolor='w')
 plt.text(0, 1, int(confusion[1][0]), ha='center', backgroundcolor='w')
 plt.text(1, 0, int(confusion[0][1]), ha='center', backgroundcolor='yellow')
@@ -101,29 +94,13 @@ ax.xaxis.set_ticks_position('none')
 ax.yaxis.set_ticks_position('none') 
 plt.imshow(confusion, cmap='Greys', vmin=0.0, vmax=1.0)
 plt.colorbar()
-plt.xticks([0, 1], ['Line present', 'Line not present'])
-plt.yticks([0, 1], ['Line detected', 'Line not detected'], rotation='vertical', va='center')
-plt.ylabel('Is the line detected?')
-plt.xlabel('Is the line present?')
+plt.xticks([0, 1], ['Spectrum present', 'Spectrum not present'])
+plt.yticks([0, 1], ['Spectrum detected', 'Spectrum not detected'], rotation='vertical', va='center')
+plt.ylabel('Is the spectrum detected?')
+plt.xlabel('Is the spectrum present?')
 plt.title(f'Confusion matrix for all lines in {test_name}\nusing {ANN_name}')
 plt.text(0, 0, np.round(confusion[0][0], 5), ha='center', backgroundcolor='w')
 plt.text(0, 1, np.round(confusion[1][0], 5), ha='center', backgroundcolor='w')
 plt.text(1, 0, np.round(confusion[0][1], 5), ha='center', backgroundcolor='yellow')
 plt.text(1, 1, np.round(confusion[1][1], 5), ha='center', backgroundcolor='w')
 plt.show()
-
-# # Save the confusion matrix as a file
-# f = open(f'{ANN_path}/confusion_{ANN_name}_{test_name}.txt', 'w')
-
-# f.write('# Rows are each individual peak.\t\t Columns are: True positives, False postives, False negatives, True negatives\n')
-
-# for i in range(len(output)):
-#     f.write(f'{output[i]}\n')
-# f.close()
-
-
-
-
-
-
-
